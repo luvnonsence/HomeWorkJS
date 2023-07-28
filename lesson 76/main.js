820f90a75ee09383e5b8b1e85dcc87e5ab93c7e1
luvnonsence <136680329+luvnonsence@users.noreply.github.com>
2023-07-28 20:37:32 +0500

    Add files via upload

---
+++ lesson 76/main.js
@@ -0,0 +1,9 @@
+const firstName = 'Анатолий'
+const lastName = 'Огоньков'
+let age = 24
+const currentState = 'Начинающий веб-разработчик'
+const homework = 'Пробую в перменные'
+
+
+// console.log(firstName, lastName, ',', age, 'лет.', currentState, homework)
+console.log(`${firstName} ${lastName}, ${age} лет. ${currentState}. ${homework}`)
---
+++ lesson 79/main.js
@@ -0,0 +1,9 @@
+const age = 24
+
+// if (age >= 18) {
+// 	console.log('Вы совершеннолетний')
+// } else {
+// 	console.log('Вам еще нет 18')
+// }
+
+age >= 18 ? console.log('Вам можно пивко') : console.log('Нельзя')
